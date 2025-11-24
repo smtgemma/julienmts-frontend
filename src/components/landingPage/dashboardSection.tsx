11a8e8b1eb9d86a3de833c32fb66b5a2042bc37a
shahidul islam <shahidulislamshohid7@gmail.com>
2025-11-24 11:20:59 +0600

    dashboard section in landing page is done

---
+++ src/components/landingPage/dashboardSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function DashboardSection() {
+    return (
+
+        <div className='lg:-mt-16'>
+            <img src="/landingPage/dashboardImage.png" alt="" className='w-full h-full' />
+        </div>
+    )
+}
+
+export default DashboardSection
