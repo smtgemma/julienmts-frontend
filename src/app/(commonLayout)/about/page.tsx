11c15c224884e92850fc1fff30ff79f2b987ef38
MMRJDevelopment <mdmahiromjansardar@gmail.com>
2025-11-27 11:50:00 +0600

    update aboutus

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Container from "@/lib/Container";
+import React from "react";
 
 function About() {
   return (
-    <div>This About page</div>
-  )
+    <div>
+      <section>
+        <div className="bg-white/60 flex w-full px-[120px] py-[80px] justify-between items-center">
+          <Container className="flex w-full justify-between items-center gap-8">
+            <h2>About Us</h2>
+            <p>Home / About Us</p>
+          </Container>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default About
+export default About;
