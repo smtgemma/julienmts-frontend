ffde52e5c574fc9f0796c3fda95c2da8ec7613cc
shahidul islam <shahidulislamshohid7@gmail.com>
2025-11-28 12:04:02 +0600

    dashboard section is done

--- src/components/landingPage/marqueeSection.tsx
+++ src/components/landingPage/marqueeSection.tsx
@@ -2,7 +2,7 @@ import Marquee from "react-fast-marquee";
 
 function MarqueeSection() {
     return (
-        <div className='border-t border-b border-[#94A3B8]'>
+        <div className='border-t border-b border-[#94A3B8] bg-white'>
             <div className="my-12"> 
                 <h3 className='md:text-2xl text-xl font-semibold text-[#2D2D2D] text-center mb-8'>Over 32k+ software  businesses growing with SalesMind</h3>
             <Marquee speed={50} gradient={false} className="flex items-center space-x-8 px-4 sm:px-8 md:px-12">
