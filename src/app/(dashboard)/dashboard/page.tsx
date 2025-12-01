b069c88a5b47c5d4d5eada7093c805ef49f7c7a3
shahidul islam <shahidulislamshohid7@gmail.com>
2025-12-01 09:28:14 +0600

    user dashboard is created start

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,17 +1,12 @@
 
-import { ChartAreaInteractive } from "@/components/chart-area-interactive"
-import { DataTable } from "@/components/data-table"
 import { SectionCards } from "@/components/section-cards"
-import data from "./data.json"
+import RecentMeetings from "@/components/recent-mettings"
 
 export default function Page() {
   return (
     <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
       <SectionCards />
-      <div className="px-4 lg:px-6">
-        <ChartAreaInteractive />
-      </div>
-      <DataTable data={data} />
+      <RecentMeetings />
     </div>
   )
 }
