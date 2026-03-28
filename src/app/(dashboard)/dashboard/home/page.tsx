51ad78fd9effd6afa299be33d36da6138ae345e5
shahidul islam <shahidulislamshohid7@gmail.com>
2026-03-28 15:09:51 +0600

    hme page is done

--- src/app/(dashboard)/dashboard/home/page.tsx
+++ src/app/(dashboard)/dashboard/home/page.tsx
@@ -15,7 +15,7 @@ export default function Page() {
     const { data: myAcount } = useMyAccountListQuery("");
     const myAccount = myAcount?.data || []
 
-    console.log(myAccount, "==================myAccount")
+    // console.log(myAccount, "==================myAccount")
 
     if (isLoading) {
         return (
@@ -31,30 +31,6 @@ export default function Page() {
             {/* active accounts  */}
             <div>
                 <h3 className="text-[#2D2D2D] text-2xl font-medium mb-6">Active Accounts</h3>
-                <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    <div className="flex flex-col border border-[#D1D6DB] p-5 rounded-xl hover:shadow-sm bg-white">
-                        {/* Avatar and Company Name */}
-                        <div className="w-12 h-12 rounded-[10px] bg-[#6E51E0] flex items-center justify-center text-white font-semibold">
-                            F
-                        </div>
-                        <h3 className="font-semibold text-[#2D2D2D] text-[18px] my-2">FastGrowth Inc.</h3>
-                        {/* Info */}
-                        <div className="flex flex-col gap-1 mb-6">
-                            <p className="text-[16px] text-[#636F85]">3 Active Opportunities</p>
-                            <p className="text-sm text-[#636F85]">Last Interaction: 5 days ago</p>
-                        </div>
-
-                        {/* Button */}
-                        <Link href="/dashboard/myAccount">
-                            <button className="shadow-sm flex items-center justify-center gap-2 w-full py-2 border border-[#D1D6DB] rounded-md text-sm font-medium text-[#0A0A0A] hover:bg-gray-50 transition-colors cursor-pointer">
-                                <ExternalLink size={14} />
-                                Open Account
-                            </button>
-                        </Link>
-                    </div>
-                </div>
-
-
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     {myAccount?.length > 0 ? (
                         myAccount.map((account: any) => (
@@ -74,11 +50,16 @@ export default function Page() {
 
                                 {/* Info */}
                                 <div className="flex flex-col gap-1 mb-6">
-                                    <p className="text-[16px] text-[#636F85]">
-                                        {account.opportunities} Active Opportunities
+                                    <p className="text-sm text-[#636F85]">
+                                        {account.description
+                                            ? account.description.slice(0, 20) + (account.description.length > 20 ? "..." : "")
+                                            : "N/A"}
                                     </p>
                                     <p className="text-sm text-[#636F85]">
-                                        Last Interaction: {account.lastInteraction}
+                                        Last Interaction:{" "}
+                                        {account.lastMeetingAt
+                                            ? new Date(account.lastMeetingAt).toLocaleDateString()
+                                            : "N/A"}
                                     </p>
                                 </div>
 
