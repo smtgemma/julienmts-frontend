5d760dad2f73d47d0e3c4206f232e89de4e53f1e
shahidul islam <shahidulislamshohid7@gmail.com>
2026-03-30 12:58:56 +0600

    base url change

--- src/components/startNewMeeting/Step5.tsx
+++ src/components/startNewMeeting/Step5.tsx
@@ -882,7 +882,8 @@ export default function LiveConversation({ handlePrev }: { handlePrev: () => voi
       const response = await fetch(
         // `http://148.230.93.55:8012/meetings/api/meeting/${meetingId}/start`,
         // `https://richelle-nonfictive-derivationally.ngrok-free.dev/meetings/api/meeting/${meetingId}/start`,
-        `http://206.162.244.134:8012/meetings/api/meeting/${meetingId}/start`,
+        // `http://206.162.244.134:8012/meetings/api/meeting/${meetingId}/start`,
+        `http://ai-julientmts.aiteamtwo.com/meetings/api/meeting/${meetingId}/start`,
         { method: "POST" }
       );
 
@@ -904,7 +905,8 @@ export default function LiveConversation({ handlePrev }: { handlePrev: () => voi
       const ws = new WebSocket(
         // `ws://148.230.93.55:8012/conversations/api/conversation/ws/live-conversation/${meetingId}`
         // `https://richelle-nonfictive-derivationally.ngrok-free.dev/conversations/api/conversation/ws/live-conversation/${meetingId}`
-        `http://206.162.244.134:8012/conversations/api/conversation/ws/live-conversation/${meetingId}`
+        // `http://206.162.244.134:8012/conversations/api/conversation/ws/live-conversation/${meetingId}`
+        `http://ai-julientmts.aiteamtwo.com/conversations/api/conversation/ws/live-conversation/${meetingId}`
       );
 
       wsRef.current = ws;
@@ -1125,7 +1127,8 @@ export default function LiveConversation({ handlePrev }: { handlePrev: () => voi
         // `http://206.162.244.134:8012/meetings/api/meeting/${meetingId}/end`,
         // `http://148.230.93.55:8012/meetings/api/meeting/${meetingId}/end`,
         // `https://richelle-nonfictive-derivationally.ngrok-free.dev/meetings/api/meeting/${meetingId}/end`,
-        `http://206.162.244.134:8012/meetings/api/meeting/${meetingId}/end`,
+        // `http://206.162.244.134:8012/meetings/api/meeting/${meetingId}/end`,
+        `http://ai-julientmts.aiteamtwo.com/meetings/api/meeting/${meetingId}/end`,
         {
           method: "POST",
         }
