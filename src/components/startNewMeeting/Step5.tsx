826d25f15e0a3c2eb2522552933f383ba5920f5f
shahidul islam <shahidulislamshohid7@gmail.com>
2026-03-30 17:32:44 +0600

    set token in cookies

--- src/components/startNewMeeting/Step5.tsx
+++ src/components/startNewMeeting/Step5.tsx
@@ -883,8 +883,8 @@ export default function LiveConversation({ handlePrev }: { handlePrev: () => voi
       const response = await fetch(
         // `http://148.230.93.55:8012/meetings/api/meeting/${meetingId}/start`,
         // `https://richelle-nonfictive-derivationally.ngrok-free.dev/meetings/api/meeting/${meetingId}/start`,
-        // `http://206.162.244.134:8012/meetings/api/meeting/${meetingId}/start`,
-        `https://ai-julientmts.aiteamtwo.com/meetings/api/meeting/${meetingId}/start`,
+        `http://206.162.244.134:8012/meetings/api/meeting/${meetingId}/start`,
+        // `https://ai-julientmts.aiteamtwo.com/meetings/api/meeting/${meetingId}/start`,
         { method: "POST" }
       );
 
@@ -906,8 +906,8 @@ export default function LiveConversation({ handlePrev }: { handlePrev: () => voi
       const ws = new WebSocket(
         // `ws://148.230.93.55:8012/conversations/api/conversation/ws/live-conversation/${meetingId}`
         // `https://richelle-nonfictive-derivationally.ngrok-free.dev/conversations/api/conversation/ws/live-conversation/${meetingId}`
-        // `http://206.162.244.134:8012/conversations/api/conversation/ws/live-conversation/${meetingId}`
-        `https://ai-julientmts.aiteamtwo.com/conversations/api/conversation/ws/live-conversation/${meetingId}`
+        `http://206.162.244.134:8012/conversations/api/conversation/ws/live-conversation/${meetingId}`
+        // `https://ai-julientmts.aiteamtwo.com/conversations/api/conversation/ws/live-conversation/${meetingId}`
       );
 
       wsRef.current = ws;
@@ -1128,8 +1128,8 @@ export default function LiveConversation({ handlePrev }: { handlePrev: () => voi
         // `http://206.162.244.134:8012/meetings/api/meeting/${meetingId}/end`,
         // `http://148.230.93.55:8012/meetings/api/meeting/${meetingId}/end`,
         // `https://richelle-nonfictive-derivationally.ngrok-free.dev/meetings/api/meeting/${meetingId}/end`,
-        // `http://206.162.244.134:8012/meetings/api/meeting/${meetingId}/end`,
-        `https://ai-julientmts.aiteamtwo.com/meetings/api/meeting/${meetingId}/end`,
+        `http://206.162.244.134:8012/meetings/api/meeting/${meetingId}/end`,
+        // `https://ai-julientmts.aiteamtwo.com/meetings/api/meeting/${meetingId}/end`,
         {
           method: "POST",
         }
