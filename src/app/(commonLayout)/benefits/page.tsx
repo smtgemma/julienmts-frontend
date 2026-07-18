3d11efafa8c8a15fe7eacb8d527487acbea91e68
shahidul islam <shahidulislamshohid7@gmail.com>
2026-07-18 09:40:28 +0600

    benefits show is done

---
+++ src/app/(commonLayout)/benefits/page.tsx
@@ -0,0 +1,13 @@
+import FeatureSection from "@/components/landingPage/FeatureSection";
+import HeaderBanner from "@/components/ui/banner/HeaderBanner";
+
+function Benefits() {
+  return (
+    <div className="relative">
+      <HeaderBanner title="Benefits" />
+      <FeatureSection />
+    </div>
+  );
+}
+
+export default Benefits;  
