6eb4d50a342d94d004bfaae01f4d35220d684ee9
shahidul islam <shahidulislamshohid7@gmail.com>
2026-07-18 14:15:19 +0600

    all issues resolve are done

--- src/components/landingPage/BashboardSection.tsx
+++ src/components/landingPage/BashboardSection.tsx
@@ -1,32 +1,4 @@
 
-// import React from 'react'
-
-// function DashboardSection() {
-//     return (
-//         <div className="max-w-[1425px] mx-auto lg:-mt-16 lg:-mb-14 relative">
-//             <img
-//                 src="/landingPage/dashboardSection/dashboardImage.png"
-//                 alt="Dashboard"
-//                 className="w-full h-auto object-cover"
-//             />
-
-    //         {/* Overlay Box */}
-    //         <div
-    //             className="
-    //   absolute bottom-2 lg:bottom-14 left-1/2 -translate-x-1/2 
-    //   bg-white
-    //   w-full
-    //   p-4 md:p-8 lg:p-11
-    //   text-center
-    // "
-    //         >
-    //         </div>
-//         </div>
-//     )
-// }
-
-// export default DashboardSection
-
 
 
 import React from 'react'
@@ -35,7 +7,8 @@ function DashboardSection() {
     return (
         <div className="max-w-[1200px] mx-auto mt-14 px-3 md:px-0">
             <img
-                src="/landingPage/dashboardSection/dashboardImage.svg"
+                // src="/landingPage/dashboardSection/dashboardImage.svg"
+                src="/landingPage/dashboardSection/dashboardImage.png"
                 alt="Dashboard"
                 className="w-full h-auto object-cover border border-[#6E51E0] border-b-0 rounded-xl rounded-b-none p-2 shadow-[0_0_100px_0_rgba(136,9,202,0.1)]"
             />
