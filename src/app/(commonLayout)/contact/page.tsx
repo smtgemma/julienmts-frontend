6455c89814501d8755c9a4cbdcea782600746ccc
shahidul islam <shahidulislamshohid7@gmail.com>
2026-07-18 10:18:20 +0600

    contact page is updated

--- src/app/(commonLayout)/contact/page.tsx
+++ src/app/(commonLayout)/contact/page.tsx
@@ -52,7 +52,7 @@ function Contact() {
           {/* Map Section */}
           <div className="w-full md:w-1/2 h-64 md:h-[447px] relative">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3689.5267489814395!2d90.3910207!3d23.8103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjPCsDQ4JzM3LjEiTiA5MMKwMjMnMjtwLjJF!5e0!3m2!1sen!2sbd!4v1234567890"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d83998.94722687619!2d2.2770198486714073!3d48.8588377391234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2sParis%2C%20France!5e0!3m2!1sen!2sfr!4v1721245678901"
               width="100%"
               height="100%"
               style={{ border: 0 }}
